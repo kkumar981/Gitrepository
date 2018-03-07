cd5d5395e77f7882382cb630fcc97965d90e35a5
Rakesh Kumar <kkumar981@gmail.com>
2018-03-07 16:11:00 +0530

    branch text changes for rebase testing.

--- DemoGitProject/src/com/rak/Main.java
+++ DemoGitProject/src/com/rak/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 			System.out.println("Hello Master branch changes");  
 			System.out.println("hello branch changes");
-			System.out.println("branch new text added");
+			System.out.println("branch new text chnages !!!");
 	
 	}
 
