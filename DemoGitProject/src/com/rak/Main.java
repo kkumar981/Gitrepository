675115a16c1a20ae16ea07ffbf86d5a7d81e3665
Rakesh Kumar <kkumar981@gmail.com>
2018-03-07 15:19:52 +0530

    Branch commite changes

--- DemoGitProject/src/com/rak/Main.java
+++ DemoGitProject/src/com/rak/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello");  
-			System.out.println("Hello world Git test ");  
-			System.out.println("Hello world Git master checkin changes "); 
-			System.out.println("Hello world Git master checkin2 changes "); 
+			System.out.println("Hello branch changes");  
+	
 	}
 
 }
