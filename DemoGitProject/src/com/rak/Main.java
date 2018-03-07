bcb7749e8160ae593cd172fdb00da31096c184d7
Rakesh Kumar <kkumar981@gmail.com>
2018-03-07 16:42:57 +0530

    Text changes in branch.

--- DemoGitProject/src/com/rak/Main.java
+++ DemoGitProject/src/com/rak/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello Master branch changes");  
-			System.out.println("hello branch changes");
-			System.out.println("branch new text chnages !!!");
-			System.out.println("Main branch texxt changes here@@");
+			
 	}
 
 }
