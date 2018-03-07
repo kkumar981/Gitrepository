cb9d65c6be0fad4760f32b5f507387c946a88838
Rakesh Kumar <kkumar981@gmail.com>
2018-03-07 13:09:36 +0530

    Master commit changes.

--- DemoGitProject/src/com/rak/Main.java
+++ DemoGitProject/src/com/rak/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 			System.out.println("Hello");  
 			System.out.println("Hello world Git test ");  
+			System.out.println("Hello world Git master checkin changes "); 
 	}
 
 }
